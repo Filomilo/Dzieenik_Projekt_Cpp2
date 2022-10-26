a23f2d01b3be12afefe63ccff4f753b89187613f
Filomilo <60512955+Filomilo@users.noreply.github.com>
2022-10-26 17:06:56 +0200

    added sqlite library
    add

--- Dziennik_library/Dziennik_library/Dziennik_library.cpp
+++ Dziennik_library/Dziennik_library/Dziennik_library.cpp
@@ -1,20 +1,19 @@
-﻿// Dziennik_library.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <stdio.h>
+#include <sqlite3.h>
 
-#include <iostream>
+int main(int argc, char* argv[]) {
+    sqlite3* db;
+    char* zErrMsg = 0;
+    int rc;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+    rc = sqlite3_open("test.db", &db);
+
+    if (rc) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return(0);
+    }
+    else {
+        fprintf(stderr, "Opened database successfully\n");
+    }
+    sqlite3_close(db);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
