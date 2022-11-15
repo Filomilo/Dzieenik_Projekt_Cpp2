48b0e9e22237568077e07977445857c65e9e494a
Filomilo <60512955+Filomilo@users.noreply.github.com>
2022-11-15 12:04:41 +0100

    added registertion page

--- Dziennik_gui/Dziennik-Gui/dziennik_guiMain.cpp
+++ Dziennik_gui/Dziennik-Gui/dziennik_guiMain.cpp
@@ -45,11 +45,20 @@ wxString wxbuildinfo(wxbuildinfoformat format)
 }
 
 //(*IdInit(dziennik_guiFrame)
+const long dziennik_guiFrame::ID_TEXTCTRLNICK = wxNewId();
+const long dziennik_guiFrame::ID_TEXTCTRLPASSREGISTER = wxNewId();
+const long dziennik_guiFrame::ID_BUTTONREGISTER = wxNewId();
+const long dziennik_guiFrame::ID_PANELCREATEADMIN = wxNewId();
+const long dziennik_guiFrame::ID_BUTTON2 = wxNewId();
+const long dziennik_guiFrame::ID_CHECKBOX1 = wxNewId();
+const long dziennik_guiFrame::ID_PANEL1 = wxNewId();
+const long dziennik_guiFrame::ID_NOTEBOOKMAIN = wxNewId();
 const long dziennik_guiFrame::ID_MENUITEM1 = wxNewId();
 const long dziennik_guiFrame::idMenuNewFIle = wxNewId();
 const long dziennik_guiFrame::idMenuOpenFile = wxNewId();
 const long dziennik_guiFrame::idMenuAbout = wxNewId();
 const long dziennik_guiFrame::ID_STATUSBAR1 = wxNewId();
+const long dziennik_guiFrame::ID_PASSWORDENTRYDIALOG1 = wxNewId();
 //*)
 
 BEGIN_EVENT_TABLE(dziennik_guiFrame,wxFrame)
@@ -63,6 +72,8 @@ dziennik_guiFrame::dziennik_guiFrame(wxWindow* parent,wxWindowID id)
                this->dziennik= new DziennikLib;
                    std::setlocale(LC_ALL, "");
     //(*Initialize(dziennik_guiFrame)
+    wxBoxSizer* BoxSizer1;
+    wxBoxSizer* BoxSizer2;
     wxMenu* Menu1;
     wxMenu* Menu2;
     wxMenuBar* MenuBar1;
@@ -70,12 +81,35 @@ dziennik_guiFrame::dziennik_guiFrame(wxWindow* parent,wxWindowID id)
     wxMenuItem* MenuItem2;
 
     Create(parent, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_STYLE, _T("wxID_ANY"));
-    SetClientSize(wxSize(1657,750));
+    SetClientSize(wxSize(1360,763));
+    NotebookMain = new wxNotebook(this, ID_NOTEBOOKMAIN, wxPoint(384,544), wxDefaultSize, 0, _T("ID_NOTEBOOKMAIN"));
+    PanelCreateAdmin = new wxPanel(NotebookMain, ID_PANELCREATEADMIN, wxPoint(441,526), wxDefaultSize, wxTAB_TRAVERSAL, _T("ID_PANELCREATEADMIN"));
+    BoxSizer1 = new wxBoxSizer(wxHORIZONTAL);
+    BoxSizer1->Add(-1,-1,1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    BoxSizer2 = new wxBoxSizer(wxVERTICAL);
+    TextCtrlNick = new wxTextCtrl(PanelCreateAdmin, ID_TEXTCTRLNICK, _("Nick"), wxDefaultPosition, wxSize(487,37), 0, wxDefaultValidator, _T("ID_TEXTCTRLNICK"));
+    BoxSizer2->Add(TextCtrlNick, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    TextCtrlPassRegister = new wxTextCtrl(PanelCreateAdmin, ID_TEXTCTRLPASSREGISTER, _("Passoword"), wxDefaultPosition, wxSize(487,37), 0, wxDefaultValidator, _T("ID_TEXTCTRLPASSREGISTER"));
+    BoxSizer2->Add(TextCtrlPassRegister, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    ButtonRegister = new wxButton(PanelCreateAdmin, ID_BUTTONREGISTER, _("Register"), wxDefaultPosition, wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTONREGISTER"));
+    BoxSizer2->Add(ButtonRegister, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    BoxSizer1->Add(BoxSizer2, 1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    BoxSizer1->Add(-1,-1,1, wxALL|wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+    PanelCreateAdmin->SetSizer(BoxSizer1);
+    BoxSizer1->Fit(PanelCreateAdmin);
+    BoxSizer1->SetSizeHints(PanelCreateAdmin);
+    Panel1 = new wxPanel(NotebookMain, ID_PANEL1, wxPoint(173,5), wxDefaultSize, wxTAB_TRAVERSAL, _T("ID_PANEL1"));
+    Panel1->Hide();
+    Button2 = new wxButton(Panel1, ID_BUTTON2, _("Label"), wxPoint(256,192), wxDefaultSize, 0, wxDefaultValidator, _T("ID_BUTTON2"));
+    CheckBox1 = new wxCheckBox(Panel1, ID_CHECKBOX1, _("Label"), wxPoint(360,472), wxDefaultSize, 0, wxDefaultValidator, _T("ID_CHECKBOX1"));
+    CheckBox1->SetValue(false);
+    NotebookMain->AddPage(PanelCreateAdmin, _("Create admin"), false);
+    NotebookMain->AddPage(Panel1, _("Page name"), false);
     MenuBar1 = new wxMenuBar();
     Menu1 = new wxMenu();
     MenuItem1 = new wxMenuItem(Menu1, ID_MENUITEM1, _("Quit\tAlt-F4"), _("Quit the application"), wxITEM_NORMAL);
     Menu1->Append(MenuItem1);
-    MenuItem3 = new wxMenuItem(Menu1, idMenuNewFIle, _("New File"), wxEmptyString, wxITEM_NORMAL);
+    MenuItem3 = new wxMenuItem(Menu1, idMenuNewFIle, _("New File\tctrl+n"), wxEmptyString, wxITEM_NORMAL);
     Menu1->Append(MenuItem3);
     MenuItem4 = new wxMenuItem(Menu1, idMenuOpenFile, _("Open File"), wxEmptyString, wxITEM_NORMAL);
     Menu1->Append(MenuItem4);
@@ -92,14 +126,17 @@ dziennik_guiFrame::dziennik_guiFrame(wxWindow* parent,wxWindowID id)
     StatusBar1->SetStatusStyles(1,__wxStatusBarStyles_1);
     SetStatusBar(StatusBar1);
     FileDialog1 = new wxFileDialog(this, _("Select file"), wxEmptyString, wxEmptyString, wxFileSelectorDefaultWildcardStr, wxFD_DEFAULT_STYLE, wxDefaultPosition, wxDefaultSize, _T("wxFileDialog"));
+    PasswordEntryDialog1 = new wxPasswordEntryDialog(this, wxEmptyString, wxEmptyString, wxEmptyString, wxCANCEL|wxCENTRE|wxOK, wxDefaultPosition);
 
+    Connect(ID_TEXTCTRLNICK,wxEVT_COMMAND_TEXT_UPDATED,(wxObjectEventFunction)&dziennik_guiFrame::OnTextCtrlNickText);
+    Connect(ID_TEXTCTRLPASSREGISTER,wxEVT_COMMAND_TEXT_UPDATED,(wxObjectEventFunction)&dziennik_guiFrame::OnTextCtrlPassRegisterText);
+    Connect(ID_BUTTONREGISTER,wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&dziennik_guiFrame::OnButtonRegisterClick);
     Connect(ID_MENUITEM1,wxEVT_COMMAND_MENU_SELECTED,(wxObjectEventFunction)&dziennik_guiFrame::OnQuit);
     Connect(idMenuAbout,wxEVT_COMMAND_MENU_SELECTED,(wxObjectEventFunction)&dziennik_guiFrame::OnAbout);
     //*)
     Connect(idMenuNewFIle,wxEVT_COMMAND_MENU_SELECTED,(wxObjectEventFunction)&dziennik_guiFrame::OnNewFile);
     Connect(idMenuOpenFile,wxEVT_COMMAND_MENU_SELECTED,(wxObjectEventFunction)&dziennik_guiFrame::OnOpenFile);
-
-
+ //   removeAllPages();
 }
 
 dziennik_guiFrame::~dziennik_guiFrame()
@@ -126,6 +163,7 @@ void dziennik_guiFrame::OnNewFile(wxCommandEvent& event)
   saveFileDialog.ShowModal();
   std::cout<<saveFileDialog.GetPath()<<std::endl;
   this->dziennik->createNewDataBase(std::string(saveFileDialog.GetPath().mbc_str()));
+  //removeAllPages();
 }
 
 void dziennik_guiFrame::OnOpenFile(wxCommandEvent& event)
@@ -136,4 +174,36 @@ void dziennik_guiFrame::OnOpenFile(wxCommandEvent& event)
     std::cout<<openFileDialog.GetPath()<<std::endl;
     this->dziennik->loadDataBase(std::string(openFileDialog.GetPath().mbc_str()));
     this->dziennik->printDataBase();
+}
+
+void dziennik_guiFrame::removeAllPages()
+{
+    int amount=NotebookMain->GetRowCount();
+    for(int i=0;i<=amount;i++)
+    {
+        NotebookMain->RemovePage(0);
+    }
+
+}
+
+void dziennik_guiFrame::OnButton1Click(wxCommandEvent& event)
+{
+
+}
+
+
+void dziennik_guiFrame::OnTextCtrlNickText(wxCommandEvent& event)
+{
+}
+
+void dziennik_guiFrame::OnTextCtrlPassRegisterText(wxCommandEvent& event)
+{
+}
+
+void dziennik_guiFrame::OnButtonRegisterClick(wxCommandEvent& event)
+{
+        std::string nick=std::string(TextCtrlNick->GetLineText(0).mbc_str());
+     std::string pass=std::string(TextCtrlPassRegister->GetLineText(0).mbc_str());
+     std::cout<<nick<<std::endl;
+     this->dziennik->addUser(nick,pass,DziennikLib::Account_types::ADMIN);
 }
--- Dziennik_gui/Dziennik-Gui/dziennik_guiMain.h
+++ Dziennik_gui/Dziennik-Gui/dziennik_guiMain.h
@@ -11,10 +11,17 @@
 #define DZIENNIK_GUIMAIN_H
 
 //(*Headers(dziennik_guiFrame)
+#include <wx/button.h>
+#include <wx/checkbox.h>
 #include <wx/filedlg.h>
 #include <wx/frame.h>
 #include <wx/menu.h>
+#include <wx/notebook.h>
+#include <wx/panel.h>
+#include <wx/sizer.h>
 #include <wx/statusbr.h>
+#include <wx/textctrl.h>
+#include <wx/textdlg.h>
 //*)
 
 #include <sqlite3.h>
@@ -35,21 +42,47 @@ class dziennik_guiFrame: public wxFrame
         void OnAbout(wxCommandEvent& event);
         void OnNewFile(wxCommandEvent& event);
         void OnOpenFile(wxCommandEvent& event);
+        void OnButton1Click(wxCommandEvent& event);
+        void OnTextCtrlNickRegisterText(wxCommandEvent& event);
+        void OnTextCtrlNickText(wxCommandEvent& event);
+        void OnTextCtrlPassRegisterText(wxCommandEvent& event);
+        void OnButtonRegisterClick(wxCommandEvent& event);
         //*)
 
+        void removeAllPages();
+
         //(*Identifiers(dziennik_guiFrame)
+        static const long ID_TEXTCTRLNICK;
+        static const long ID_TEXTCTRLPASSREGISTER;
+        static const long ID_BUTTONREGISTER;
+        static const long ID_PANELCREATEADMIN;
+        static const long ID_BUTTON2;
+        static const long ID_CHECKBOX1;
+        static const long ID_PANEL1;
+        static const long ID_NOTEBOOKMAIN;
         static const long ID_MENUITEM1;
         static const long idMenuNewFIle;
         static const long idMenuOpenFile;
         static const long idMenuAbout;
         static const long ID_STATUSBAR1;
+        static const long ID_PASSWORDENTRYDIALOG1;
         //*)
 
         //(*Declarations(dziennik_guiFrame)
+        wxButton* Button2;
+        wxButton* ButtonRegister;
+        wxCheckBox* CheckBox1;
         wxFileDialog* FileDialog1;
+        wxMenu Menu3;
         wxMenuItem* MenuItem3;
         wxMenuItem* MenuItem4;
+        wxNotebook* NotebookMain;
+        wxPanel* Panel1;
+        wxPanel* PanelCreateAdmin;
+        wxPasswordEntryDialog* PasswordEntryDialog1;
         wxStatusBar* StatusBar1;
+        wxTextCtrl* TextCtrlNick;
+        wxTextCtrl* TextCtrlPassRegister;
         //*)
 
         DECLARE_EVENT_TABLE()
